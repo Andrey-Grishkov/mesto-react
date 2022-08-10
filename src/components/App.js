1ee56fea713598431d99f9999650bc73d2e9f1dc
Андрей Гришков <andreygorm@gmail.com>
2022-08-10 18:24:45 +0300

    add catch to api reqest

--- src/components/App.js
+++ src/components/App.js
@@ -87,14 +87,14 @@ function App() {
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
       setCards((state) => state.map((c) => (c._id === card._id ? newCard : c)));
-    });
+    }).catch((err) => console.log(`Ошибка: ${err}`));
   }
 
   function handleCardDelete(card) {
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api.deleteCard(card._id).then(() => {
       setCards((state) => state.filter((c) => c._id !== card._id));
-    });
+    }).catch((err) => console.log(`Ошибка: ${err}`));
   }
 
   const handleAddPlaceSubmit = (data) => {
